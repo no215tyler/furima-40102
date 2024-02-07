198e50b0930814b521e1d18c0166b47d94c147a5
no215tyler <no.215.tyler@gmail.com>
2024-02-07 15:34:43 +0900

    Formオブジェクトを用いた購入登録機能のテストコード（PAYJPの機能を除く）

---
+++ spec/factories/order_shipping_addressese.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :order_shipping_address do
+    post_code { "#{Faker::Number.number(digits: 3)}-#{Faker::Number.number(digits: 4)}" }
+    prefecture_id { Faker::Number.between(from: 1, to: 47) }
+    city { Faker::Address.city }
+    address { Faker::Address.street_address }
+    building { Faker::Address.secondary_address }
+    phone_number { Faker::Number.leading_zero_number(digits: rand(10..11)) }
+  end
+end
